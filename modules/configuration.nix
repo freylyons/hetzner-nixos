1066fd1bafc340d2a131ef7e1f923241e9705ed8
Frey <freylyons@hotmail.co.uk>
2025-04-29 18:02:54 +0100

    added ceph to system packages

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -85,6 +85,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    ceph
     git
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
